cadab0157d55bdabff5455558b7362dacb02fc53
szkol22_06_2026 <borowieckikamil27@gmail.com>
2026-06-29 00:38:59 +0200

    dzien 4 kalk i fig
    dzien 4 kalk i fig

---
+++ szkolenie22_06_2026/4_dzien/figury.py
@@ -0,0 +1,8 @@
+def pole_kwadratu(a:int):
+    return a*a
+def pole_kola(r: int):
+    return 3.14 * r ** 2
+
+
+if __name__ == "__main__":
+    print(pole_kwadratu(5))
---
+++ szkolenie22_06_2026/4_dzien/kalkulator.py
@@ -0,0 +1,13 @@
+def dodaj(a:int,b:int):
+    return a+b
+
+def odejmij(a:int,b:int):
+    return a -b 
+
+def mnoz(a:int,b:int):
+    return a * b
+
+if __name__ == "__main__":
+    print(dodaj(2,5))
+    print(odejmij(10,5))
+    print(mnoz(3,3))
